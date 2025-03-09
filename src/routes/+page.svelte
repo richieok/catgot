c666af01ecd7daa86a5f3f7e7324c91c67ade9ad
AJ <richie.okoro@gmail.com>
2025-03-09 10:33:36 +0100

    First commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<div>
+    <h1>Get Ready to kick ass</h1>
+</div>
+
+<style>
+    div {
+        display: grid;
+        place-content: center;
+        height: 100vh;
+    }
+</style>
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,14 @@
+<div>
+    <h1><span>KAPOOW!</span><br> That Ass got kicked</h1>
+</div>
+
+<style>
+    div {
+        display: grid;
+        place-content: center;
+        height: 100vh;
+    }
+    span {
+        color: blueviolet;
+    }
+</style>
